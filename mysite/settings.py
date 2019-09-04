ec53c1191bc2927aec3b5171e3456138ad9556cc
Loki <bounthienglocky@gmail.com>
2019-09-05 01:34:00 +0200

    saved

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,11 +76,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blogdb1',
-        'USER':'root',
-        'PASSWORD': 'toor',
-        'HOST': 'localhost'#'den1.mysql3.gear.host',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        # 'NAME': 'blogdb1',
+        # 'USER':'root',
+        # 'PASSWORD': 'toor',
+        # 'HOST': 'localhost'#'den1.mysql3.gear.host',
         # 'OPTIONS': {'read_default_file':'/etc/mysql/my.cnf'},
     }
 }
